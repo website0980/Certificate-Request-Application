e2be6c3cc51cf2934347bb3c104a399535e574f6
website0980 <promotionalwork098@gmail.com>
2026-08-06 08:19:49 +0800

    Add files via upload

--- autoload.php
+++ autoload.php
@@ -1,19 +1,21 @@
 <?php
-require_once __DIR__ . '/fpdf/fpdf.php';
 
-spl_autoload_register(function (string $class) {
-    $prefix = 'setasign\\Fpdi\\';
-    $baseDir = __DIR__ . '/fpdi-full/src/';
-    $prefixLength = strlen($prefix);
+/**
+ * This file is part of FPDI
+ *
+ * @package   setasign\Fpdi
+ * @copyright Copyright (c) 2026 Setasign GmbH & Co. KG (https://www.setasign.com)
+ * @license   http://opensource.org/licenses/mit-license The MIT License
+ */
 
-    if (strncmp($prefix, $class, $prefixLength) !== 0) {
-        return;
-    }
+spl_autoload_register(static function ($class) {
+    if (strpos($class, 'setasign\Fpdi\\') === 0) {
+        $filename = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 14)) . '.php';
+        $fullpath = __DIR__ . DIRECTORY_SEPARATOR . $filename;
 
-    $relativeClass = substr($class, $prefixLength);
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
-
-    if (file_exists($file)) {
-        require_once $file;
+        if (is_file($fullpath)) {
+            /** @noinspection PhpIncludeInspection */
+            require_once $fullpath;
+        }
     }
 });
